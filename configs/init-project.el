f863815c44e11453b0d409980dd02d9a59e22631
Andrew Megli <amegli@gmail.com>
2024-10-27 12:52:04 -0400

    Add eat, some new bindings, and some experimental changes

--- configs/init-project.el
+++ configs/init-project.el
@@ -7,6 +7,13 @@
     (setq projectile-project-search-path '("~/Development")))
   (setq projectile-switch-project-action #'projectile-dired))
 
+(use-package perspective
+  :ensure t
+  :custom
+  (persp-mode-prefix-key (kbd "C-c M-p")) 
+  :init
+  (persp-mode))
+
 (use-package dired-single)
 (use-package all-the-icons-dired
   :hook (dired-mode . all-the-icons-dired-mode))
