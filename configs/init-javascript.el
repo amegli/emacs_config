8315832fc1f3e7bd8959b330a4705e8076297335
Andrew Megli <andrew.megli@esquiresolutions.com>
2025-03-21 17:28:15 -0400

    Add web-mode and apply to html-ish file types

--- configs/init-javascript.el
+++ configs/init-javascript.el
@@ -1,5 +1,10 @@
 (use-package json-mode)
 
+(use-package web-mode
+  :config
+  (setq web-mode-content-types-alist
+        '(("ejs" . "\\.ssp\\'"))))
+
 (use-package jest-test-mode
   :ensure t
   :commands jest-test-mode
@@ -8,5 +13,6 @@
 (use-package jest)
 
 (add-to-list 'auto-mode-alist '("\\.toast\\'" . json-mode))
+(add-to-list 'auto-mode-alist '("\\.ssp\\'" . web-mode))
 
 (provide 'init-javascript)
