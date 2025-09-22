c3cf807b21986e3202ed45f04c4ec3ccc4ec526c
Andrew Megli <andrew.megli@esquiresolutions.com>
2025-09-21 22:47:16 -0400

    Expand org todo statuses, remove magit features for efficiency

--- configs/init-git.el
+++ configs/init-git.el
@@ -1,6 +1,11 @@
 
 (use-package magit
-  :commands (magit-status magit-get-current-branch))
+  :commands (magit-status magit-get-current-branch)
+	:config
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-pushremote)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-pushremote)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-upstream)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-upstream-or-recent))
 
 (use-package git-gutter
   :init (global-git-gutter-mode t))
