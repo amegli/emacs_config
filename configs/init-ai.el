e774fd87c437fb377fd63ed22c4f68ea66f27f76
Andrew Megli <amegli@gmail.com>
2026-03-15 13:20:28 -0400

    Add agent-shell and claude code configuration

--- configs/init-ai.el
+++ configs/init-ai.el
@@ -7,16 +7,18 @@
   (setq gptel-include-tool-results t))
 (add-hook 'gptel-post-response-functions 'gptel-end-of-response)
 
-(use-package copilot
-  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("dist" "*.el"))
-  :config
-  (setq copilot-idle-delay 60)
-  (global-set-key (kbd "C-'") 'copilot-complete)
-  :hook (prog-mode-hook . copilot-mode)
-  :bind (:map copilot-completion-map
-              ("<tab>" . 'copilot-accept-completion)
-              ("TAB" . 'copilot-accept-completion)
-              ("C-TAB" . 'copilot-accept-completion-by-word)
-              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+(use-package agent-shell
+  :ensure t
+	:ensure-system-package
+  ((claude-agent-acp . "npm install -g @zed-industries/claude-agent-acp"))
+	:config
+  (evil-define-key 'insert agent-shell-mode-map (kbd "RET") #'newline)
+  (evil-define-key 'normal agent-shell-mode-map (kbd "RET") #'comint-send-input))
+
+(setq agent-shell-anthropic-authentication
+      (agent-shell-anthropic-make-authentication :login t))
+(setq agent-shell-anthropic-claude-environment
+      (agent-shell-make-environment-variables :inherit-env t))
+(setq agent-shell-session-strategy 'prompt)
 
 (provide 'init-ai)
